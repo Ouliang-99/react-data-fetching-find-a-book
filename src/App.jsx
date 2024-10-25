5d6d14d6010da1bcb9a9b9d4362431d1eaf69508
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-25 17:19:36 +0700

    feat: search function finding a book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [resultInputSearch, setResultInputSearch] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  const getData = async (input) => {
+    try {
+      const books = await axios.get(
+        `https://openlibrary.org/search.json?q=${input}`
+      );
+      setResultInputSearch(books.data.docs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (inputSearch)
+    getData(inputSearch);
+  }, [inputSearch]);
+
+  const handleInputSearch = (event) => {
+    setInputSearch(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        className="search"
+        value={inputSearch}
+        onChange={handleInputSearch}
+        placeholder="Search here"
+      />
+      <ul>
+        {resultInputSearch.map((text, index) => {
+          return <li key={index}>{text.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
